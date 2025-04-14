0b52eca416e129fff1e479ef95e0a649abb7e7df
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2025-04-14 17:45:31 +0900

    スマホレイアウト修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
-        darkblue: "#00008b",
-        cadetblue: "#5f9ea0",
-        whitesmoke: "#f5f5f5",
-      },
-      screens: {
-        "3xl": "1600px",
+        darkblue: '#00008b',
+        cadetblue: '#5f9ea0',
+        whitesmoke: '#f5f5f5',
       },
     },
   },
