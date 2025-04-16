6cde8a3499ebf269087c95478bde51f2df322a05
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2025-04-16 22:37:26 +0900

    TopButonコンポーネント作成

--- src/components/TopButon.tsx
+++ src/components/TopButon.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { BsArrowUpSquareFill } from 'react-icons/bs';
 
 function TopButon() {
-  return <div>TopButon</div>;
+  const [scrolling, setScrolling] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <div className={scrolling ? 'fixed bottom-[0.5rem] right-[0.7rem] z-50 ' : 'hidden'}>
+      <a href="#home">
+        <BsArrowUpSquareFill className="text-cadetblue shadow-sm " size={40} />
+      </a>
+    </div>
+  );
 }
 
 export default TopButon;
