e41f76a163d9a3bbdfa83a9509aae724ee632dc3
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2025-04-21 03:46:12 +0900

    SideMenuコンポーネント微修正（コピーライトの位置修正）

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -47,62 +47,60 @@ function SideMenu() {
   }, [isOpen]);
 
   return (
-    <>
-      <section
-        className={`fixed top-0 left-0 z-50 flex h-screen max-w-[300px] flex-col justify-between px-[20px] py-10 lg:px-[80px] max-lg:max-w-[146px] bg-white border-r ${isOpen ? 'max-lg:block' : 'max-md:hidden'}`}
-      >
-        <div className="flex w-full flex-1 flex-col gap-10 justify-between items-center">
-          {/* 上 */}
-          <div className="flex flex-col justify-center items-center  gap-2">
-            <Image src={'/assets/logo.svg'} width={200} height={200} alt={'NextLayer Image'} />
-            {/* <p className="font-bold text-darkblue">NextLayer</p> */}
-          </div>
-          {/* 中 サイドメニュー*/}
-          <div>
-            {sideMenuLinks.map((link: SideMenuLink) => {
-              const isActive = activeLink === link;
-              return (
-                <a
-                  key={link.label}
-                  href={link.route}
-                  className={`relative flex justify-center rounded-lg p-3 ${isActive ? 'bg-cadetblue text-white' : ''}`}
-                  onClick={() => handleLinkClick(link)}
-                >
-                  <p>{link.label}</p>
-                </a>
-              );
-            })}
-          </div>
-          {/* 下 */}
-          <div className="flex flex-col items-center justify-center text-center">
-            <div className="flex flex-col lg:flex-row items-center gap-4 mb-3">
-              <a
-                href=""
-                target="_blank"
-                className="bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white"
-              >
-                <AiFillGithub />
-              </a>
-              <a
-                href=""
-                target="_blank"
-                className="bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white"
-              >
-                <AiFillLinkedin />
-              </a>
-              <a
-                href=""
-                target="_blank"
-                className="bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white"
-              >
-                <FaDev />
-              </a>
-              <p className="max-lg:hidden">© 2025 NextLayer</p>
-            </div>
-          </div>
+    <section
+      className={`fixed top-0 left-0 z-50 flex h-screen max-w-[300px] flex-col justify-between px-[20px] py-10 lg:px-[80px] max-lg:max-w-[146px] bg-white border-r ${isOpen ? 'max-lg:block' : 'max-md:hidden'}`}
+    >
+      <div className="flex w-full flex-1 flex-col gap-10 justify-between items-center">
+        {/* 上 */}
+        <div className="flex flex-col justify-center items-center  gap-2">
+          <Image src={'/assets/logo.svg'} width={200} height={200} alt={'NextLayer Image'} />
+          {/* <p className="font-bold text-darkblue">NextLayer</p> */}
         </div>
-      </section>
-    </>
+        {/* 中 サイドメニュー*/}
+        <div>
+          {sideMenuLinks.map((link: SideMenuLink) => {
+            const isActive = activeLink === link;
+            return (
+              <a
+                key={link.label}
+                href={link.route}
+                className={`relative flex justify-center rounded-lg p-3 ${isActive ? 'bg-cadetblue text-white' : ''}`}
+                onClick={() => handleLinkClick(link)}
+              >
+                <p>{link.label}</p>
+              </a>
+            );
+          })}
+        </div>
+        {/* 下 */}
+        <div className="flex flex-col items-center justify-center text-center">
+          <div className="flex flex-col lg:flex-row items-center gap-4 mb-3">
+            <a
+              href=""
+              target="_blank"
+              className="bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white"
+            >
+              <AiFillGithub />
+            </a>
+            <a
+              href=""
+              target="_blank"
+              className="bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white"
+            >
+              <AiFillLinkedin />
+            </a>
+            <a
+              href=""
+              target="_blank"
+              className="bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white"
+            >
+              <FaDev />
+            </a>
+          </div>
+          <p className="max-lg:hidden">© 2025 NextLayer</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
